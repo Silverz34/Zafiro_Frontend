a909528149d51f6efffa3e1276ffdeb330ef5842
Silverz34 <pradogordillomichael@gmail.com>
2026-02-15 22:11:40 -0600

    colores, tipografia base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Nunito} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
+  display: "swap"
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geistMono = Nunito({
+  variable: "--font-nunito",
   subsets: ["latin"],
+  display: "swap"
 });
 
 export const metadata: Metadata = {
