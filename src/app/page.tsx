52e836ed7dd5b76c5833b737baa64a51646be11c
Silverz34 <pradogordillomichael@gmail.com>
2026-02-15 22:45:33 -0600

    ya lo solucione

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col font-sans dark:bg-[var(--background)]">
       <header className="w-full py-6 px-8 flex justify-between items-center max-w-7xl mx-auto">
-        <div>
-          <Image src="/logo.png" alt="Zafiro Logo" width={32} height={32} className="h-8 w-auto" />
+        <div className="flex items-center gap-2">
+          <Image src="/Logo_zafiro.png" alt="Zafiro Logo" width={500} height={500} className="h-30 w-auto" />
         </div>
         <div className="flex gap-4">
           <Link href="/login" className="px-4 py-2 text-sm hover:text-blue-400 transition-colors">
