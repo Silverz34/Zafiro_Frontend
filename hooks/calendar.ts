007363ac26e4745e8afb16ddbeb4329453d671ce
Silverz34 <pradogordillomichael@gmail.com>
2026-02-22 21:21:44 -0600

    mapeo de datos

--- hooks/calendar.ts
+++ hooks/calendar.ts
@@ -14,5 +14,33 @@ export function CalendarLogic(currentDate: Date, rawEvents: GoogleEvent[]){
     }, [currentDate]);
 
     const hours = Array.from({length: 24}).map((_, i)=> i);
-    
-}
+    const getProcessed = (date: Date) =>{
+        if(!rawEvents) return[];
+        const dayEvent = rawEvents.filter(event =>{
+            if (!event.start.dateTime) return false;
+            const eventDate = new Date(event.start.dateTime);
+            return(
+                eventDate.getDate() === date.getDate() &&
+                eventDate.getMonth() === date.getMonth() &&
+                eventDate.getFullYear() === date.getFullYear()
+            );
+        });
+
+        return dayEvent.map((event: GoogleEvent) => {
+            const start = new Date(event.start.dateTime!);
+            const end = new Date( event.end.dateTime!);
+
+            const startHour = start.getHours() + start.getMinutes()/60;
+            const duration = (end.getTime()- start.getTime()) / (1000 * 60 * 60 );
+            return {
+                ...event,
+                positionStyle: {
+                    top: `${startHour * 80}px`,
+                    height: `${duration * 80}px`,
+                },
+                formattedTime: start.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+            };
+        });
+    }
+    return {weekDay, hours, getProcessed};
+};
