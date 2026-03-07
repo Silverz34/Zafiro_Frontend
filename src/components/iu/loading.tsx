44475cc0b0d6ac146e645a9475192c7690dd5359
Silverz34 <pradogordillomichael@gmail.com>
2026-03-07 14:46:18 -0600

    cambios, eliminacion de daisy

--- src/components/iu/loading.tsx
+++ src/components/iu/loading.tsx
@@ -1,5 +1,5 @@
 import { FondoHero } from "../landing/Hero/fondo-hero"
-import "daisyui"; 
+import { Loader } from "lucide-react";
 
 export default function Loading(){
     return(
@@ -9,7 +9,7 @@ export default function Loading(){
             <div className="absolute inset-0 bg-linear-to-t from-[#010112] via-[#010112]/60 to-[#010112]/30 backdrop-blur-[2px]"></div>
         </div>
         <div className="relative z-10 flex flex-col items-center gap-5 px-12 py-10 rounded-2xl">
-            <span className="loading loading-dots loading-xl text-blue-600/70 scale-225"></span>
+            <Loader className="w-20 h-20 text-blue-500 animate-spin" />
             <p className="text-blue-500/80 text-2xl py-6 font-medium tracking-wide animate-pulse">
                 Cargando...
             </p>
