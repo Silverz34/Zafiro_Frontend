10d6ca93436d79a41b5d553d2bdec2fa08ae715a
Silverz34 <pradogordillomichael@gmail.com>
2026-03-09 00:51:38 -0600

    pre esquema de datos usados en el formulario de creacion de actividad

---
+++ lib/types/FormActividad.ts
@@ -0,0 +1,16 @@
+import { TipoOcurrencia } from "../../hooks/Ocurrencia";
+
+export type PrioridadType = "Alta" | "Media" | "Baja";
+//datos para el formulario que despues seran validados antes de enviar por el squema de zod
+export interface FormActividad{
+    titulo: string;
+    fecha: string;
+    horaInicio: string;
+    horaFin: string;
+
+    isAllDay: boolean;
+    recurrencia: TipoOcurrencia;
+    remider: string;
+    ocupacion: "opaque" | "transparent";
+    prioridad: PrioridadType;
+}
