6e4707edba72fd255d6bffae4dad3d78a7322026
Silverz34 <pradogordillomichael@gmail.com>
2026-03-09 00:41:31 -0600

    restriccciones para enviar una actividad

--- interfaces/Evento.ts
+++ interfaces/Evento.ts
@@ -3,12 +3,15 @@ import { z } from "zod";
 export const GoogleSchema= z.object({
     dateTime: z.string().optional(),
     date: z.string().optional()
-})
+}).refine(
+  data => data.dateTime || data.date,
+  { message: "Se requiere dateTime o date" }
+);
 
 export const GEventSchema = z.object(
     {
         id: z.string(),
-        summary: z.string(),
+          summary: z.string().min(2, "El título es obligatorio"),
         start: GoogleSchema,
         end: GoogleSchema
     }
