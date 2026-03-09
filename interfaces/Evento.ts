41a71e744b9b954fa387c34ae11a173c622debdc
Silverz34 <pradogordillomichael@gmail.com>
2026-03-09 01:32:38 -0600

    creacion del metodo para enviar datos, se espera validacion con zod si un campo es incorrecto

--- interfaces/Evento.ts
+++ interfaces/Evento.ts
@@ -10,10 +10,10 @@ export const GoogleSchema= z.object({
 
 export const GEventSchema = z.object(
     {
-        id: z.string(),
-          summary: z.string().min(2, "El título es obligatorio"),
-        start: GoogleSchema,
-        end: GoogleSchema
+      id: z.string().optional(),
+      summary: z.string().min(2, "El título es obligatorio"),
+      start: GoogleSchema,
+      end: GoogleSchema
     }
 );
 
