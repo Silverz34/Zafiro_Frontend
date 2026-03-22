46ef99af046797e317a4592ec573f63bea2ebb30
Silverz34 <pradogordillomichael@gmail.com>
2026-03-21 22:48:04 -0600

    semaforo en vistas mes

--- src/components/viewsCalendar/MonthView.tsx
+++ src/components/viewsCalendar/MonthView.tsx
@@ -1,13 +1,12 @@
 'use client'
 import { CalendarLogic } from "../../../hooks/calendar/calendar"
 import type { ViewProps } from "../../../interfaces/types/props";
-import { useEtiquetas } from "../../../hooks/useEtiquetas";
+import { calcularSemaforo } from "../../../hooks/calendar/semaforo";
 
 export default function MonthView({ currentDate, events, onOpenModal, onEventClick }: ViewProps) {
 
     const { days, getProcessed } = CalendarLogic(currentDate, events, 'mes');
     const dayNames = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
-    const { etiquetas } = useEtiquetas();
 
     return (
         <div className="flex flex-col h-full bg-[#100F1D] rounded-xl border border-gray-800 overflow-hidden text-white">
@@ -24,21 +23,31 @@ export default function MonthView({ currentDate, events, onOpenModal, onEventCli
                     const processedEvents = getProcessed(date);
                     const isToday = date.toDateString() === new Date().toDateString();
                     const isCurrentMonth = date.getMonth() === currentDate.getMonth();
+                    const colorSemaforo = calcularSemaforo(processedEvents);
+                    const headerClass = 
+                      colorSemaforo === 'rojo' ? 'bg-[#AB3535] text-white border-b border-[#8A2525]' :
+                      colorSemaforo === 'naranja' ? 'bg-[#E2761F] text-white border-b border-[#C16215]' :
+                      colorSemaforo === 'verde' ? 'bg-[#2FA941] text-white border-b border-[#228531]' :
+                      'bg-transparent text-gray-300'; 
 
                     return (
                         <div
                             key={index}
                             onClick={onOpenModal}
-                            className={`border-r border-b border-gray-800 p-1 flex flex-col gap-1 min-h-25 hover:bg-gray-800/30 transition-colors cursor-pointer ${!isCurrentMonth ? 'bg-[#0a0914] opacity-50' : ''
-                                }`}
+                            className={`border-r border-b border-gray-800 flex flex-col min-h-25 hover:bg-gray-800/30 transition-colors cursor-pointer ${
+                                !isCurrentMonth ? 'bg-[#0a0914] opacity-50' : ''
+                            }`}
                         >
-                            <div className="flex justify-center mb-1">
-                                <span className={`text-xs font-medium w-6 h-6 flex items-center justify-center rounded-full mt-1 ${isToday ? 'bg-blue-600 text-white font-bold shadow-md shadow-blue-900/50' : 'text-gray-300'
+                            <div className={`flex justify-center py-1 transition-colors duration-300 ${headerClass}`}>
+                                <span className={`text-xs w-6 h-6 flex items-center justify-center rounded-full ${
+                                    isToday 
+                                        ? (colorSemaforo === 'normal' ? 'bg-blue-600 font-bold text-white shadow-md shadow-blue-900/50' : 'bg-white/30 font-bold text-white') 
+                                        : 'font-medium'
                                     }`}>
                                     {date.getDate()}
                                 </span>
                             </div>
-                            <div className="flex flex-col gap-1 overflow-y-auto max-h-full scrollbar-none pb-1">
+                            <div className="flex flex-col gap-1 overflow-y-auto max-h-full scrollbar-none p-1">
                                 {processedEvents.map(event => (
                                     <div
                                         key={event.id}
