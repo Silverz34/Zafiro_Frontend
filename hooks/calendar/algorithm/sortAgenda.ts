5eafa960c709f3af1d6fb2ac2a9d82dad17fa1f6
Yael <243678@ids.upchiapas.edu.mx>
2026-04-05 00:45:21 -0600

    Se añaden métodos para obtener fechas y conseguir las actividades desde el front por que la api me confunde y ni encontré el método para filtrar por fecha mientras que este ya lo tiene

--- hooks/calendar/algorithm/sortAgenda.ts
+++ hooks/calendar/algorithm/sortAgenda.ts
@@ -1,8 +1,48 @@
-import { AlgorithmResponse, ConfigType } from "../../../interfaces/Algorithm"
+import { AgendaArray, AgendaArrayType, AlgorithmRequest, AlgorithmResponse, Config, ConfigType } from "../../../interfaces/Algorithm"
+import { lecturaActividad } from "../../../interfaces/Preview"
+import { fetchGoogleEvents } from "../../../lib/CrudActividad/fetchActividad"
 import { apiGet, apiPost } from "../../../lib/sincronizacion/apiClient"
 import { ApiError } from "../../../lib/sincronizacion/apiError"
 
 export class algorithmHook {
+    private createDates(limit: number): { today: string, end: string } {
+        const today = new Date()
+        const currentYear = today.getFullYear()
+        const currentMonth = today.getMonth()
+        const currentDay = today.getDay()
+        console.log(today.toISOString(), currentYear, currentMonth, currentDay)
+        const end = new Date(currentYear, currentMonth, currentDay + limit)
+
+        const todayStr = today.toISOString()
+        const endStr = end.toISOString()
+        
+        return { today:todayStr, end:endStr }
+    }
+
+    private parseActivities(activities: lecturaActividad[] ): AgendaArray {
+        if (activities.length == 0) {
+            return []
+        }
+        const activitiesParsed:AgendaArray = activities.map((act) => {
+            return {
+                id: act.id,
+                summary: act.summary,
+                start: act.start,
+                end: act.end,
+                transparency: act.transparency ?? 'opaque',
+                reminders: act.reminders ?? { useDefault: true },
+                extras: {
+                    prioridad: act.prioridadValor ?? 'media',
+                    etiquetas: {
+                        etiqueta: act.etiqueta?.id ?? 0,
+                        color: act.etiqueta?.color ?? '#555555'
+                    }
+                }
+            }
+        })
+        return activitiesParsed
+    }
+
     public async verifyAlgorithm():Promise<boolean | null>{
         try {
             const response = await apiGet<any>("/api/algorithm/health")
@@ -23,12 +63,35 @@ export class algorithmHook {
         }
     }
 
-    public async sortAgenda(payload: object):Promise<AlgorithmResponse | null>  {
+    public async sortAgenda(data: object):Promise<AlgorithmResponse | null>  {
         try {
-            const payloadParsed = ConfigType.parse(payload)
+            const configParsed: Config = ConfigType.parse(data)
+            console.log(configParsed)
+
+            const { today, end } = this.createDates(configParsed.dias_contemplados)
+            const activities: lecturaActividad[] = await fetchGoogleEvents(today,end) ?? []
+            console.log(today, end, activities)
+            const activitiesParsed: AgendaArray = this.parseActivities(activities)
+            if (activitiesParsed.length == 0) {
+                console.error("[algorithmSort] No se recibieron actividades")
+                return null
+            }
+
+            const payload: AlgorithmRequest = {
+                config: configParsed,
+                calendar: {
+                    defaultReminders: [{
+                        method: 'email',
+                        minutes: 15
+                    }],
+                    items: activitiesParsed
+                }
+            }
+            console.log(payload)
+            
             const response = await apiPost<AlgorithmResponse>(
                 '/api/algorithm/sort',
-                payloadParsed
+                payload
             )
             if (!response.success){
                 console.error('[algorithmSort] Error al reordenar las actividades.')
