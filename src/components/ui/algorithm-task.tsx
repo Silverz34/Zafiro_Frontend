84a8cb4f724cb75a73879864e5993cbafe5e597e
Yael <243678@ids.upchiapas.edu.mx>
2026-04-08 03:05:57 -0600

    se cambia a que solo muestre la hora de inicio y fin si sí se agendó la tarea
    ya con esto me voy a mimir

--- src/components/ui/algorithm-task.tsx
+++ src/components/ui/algorithm-task.tsx
@@ -9,10 +9,12 @@ export default function Task({tarea, isScheduled}: TaskProps) {
     return (
         <div className={`${isScheduled ? 'bg-[#2FA941]' : 'bg-[#AB3535]'} p-3 rounded-2xl my-2`}>
             <p>Tarea: {tarea.summary}</p>
-            <div>
-                <p>Horario nuevo:</p>
-                <p>{tarea.start.date ?? tarea.start.dateTime} - {tarea.end.date ?? tarea.end.dateTime}</p>
-            </div>
+            {isScheduled && (
+                <div>
+                    <p>Horario nuevo:</p>
+                    <p>{tarea.start.date ?? tarea.start.dateTime} - {tarea.end.date ?? tarea.end.dateTime}</p>
+                </div>
+            )}
         </div>
     )
 }
